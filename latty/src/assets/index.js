a8afbd126c85367ea8bfcc0828231cea4ed81030
SEOg-u <seogyeoul1898@gmail.com>
2022-12-15 23:39:19 +0900

    Update Assets Index

--- latty/src/assets/index.js
+++ latty/src/assets/index.js
@@ -1,1 +1,3 @@
 export { default as sadFace } from "./img/SadFace.png";
+export { default as warningXBt } from "./img/WarningXBt.png";
+export { default as exclamationMark } from "./img/ExclamationMark.png";
