c22108d76031be4f3729237ac3a9ccecd6a374b3
SEOg-u <seogyeoul1898@gmail.com>
2022-12-18 12:26:35 +0900

    Update Practice

--- latty/src/Practice.js
+++ latty/src/Practice.js
@@ -1,10 +1,19 @@
-import Fail from "./components/modal/warning/Fail";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Practice = () => {
+  // 변수 선언
+  const notify = () => {
+    toast.success("로그인을 성공했습니다.", {
+      position: toast.POSITION.TOP_RIGHT,
+      autoClose: 1000,
+    });
+  };
   return (
-    <>
-      <Fail />
-    </>
+    <div>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer />
+    </div>
   );
 };
 
