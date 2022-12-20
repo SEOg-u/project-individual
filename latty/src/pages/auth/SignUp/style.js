950f3ad1fac71d67e333b5abb527990f51a5e21d
SEOg-u <seogyeoul1898@gmail.com>
2022-12-20 23:38:10 +0900

    Create pages/auth/SignUp/style

---
+++ latty/src/pages/auth/SignUp/style.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+// styled-components
+export const Body = styled.div`
+  margin: 11px 0 0 25px;
+  width: 550px;
+`;
+export const Header = styled.div`
+  height: 33px;
+  border-bottom: 1px solid #d9d9d9;
+`;
+export const Title = styled.p`
+  font-size: 17px;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 500;
+`;
+
+export const Nav = styled.div`
+  margin-top: 8px;
+  height: 55px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fafafa;
+  border: 1px solid #d9d9d9;
+`;
+export const Number = styled.p`
+  font-size: 16px;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 600;
+  color: #444444;
+`;
+export const Input = styled.input`
+  padding: 0 10px 0 10px;
+  height: 25px;
+  width: 181px;
+  font-size: 13px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  background: #ffffff;
+  border: 1px solid #d9d9d9;
+`;
+export const Bt = styled.button`
+  width: 25px;
+  height: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #e5e8d4;
+  border: 1px solid #6c7139;
+`;
+export const Img2 = styled.img`
+  width: 13px;
+  height: 13px;
+`;
+
+export const SearchBox = styled.div`
+  margin-top: 8px;
+  width: 550px;
+  height: 40px;
+  display: flex;
+  background: #e5e8d4;
+  border-width: 2px 1px 1px 1px;
+  border-style: solid;
+  border-color: #6c7139;
+`;
+export const P = styled.p`
+  font-size: 14px;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 500;
+`;
