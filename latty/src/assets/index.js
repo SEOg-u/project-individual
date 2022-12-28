90d0511ac9199bed029eeee04c37436f1e200cf2
SEOg-u <seogyeoul1898@gmail.com>
2022-12-28 22:30:34 +0900

    Update Assets Index

--- latty/src/assets/index.js
+++ latty/src/assets/index.js
@@ -5,3 +5,11 @@ export { default as logo } from "./img/Logo.png";
 export { default as nextBt } from "./img/NextBt.png";
 export { default as beforeBt } from "./img/BeforeBt.png";
 export { default as search } from "./img/Search.png";
+export { default as headerLine } from "./img/HeaderLine.png";
+export { default as colon } from "./img/Colon.png";
+
+// icons
+export { default as icon1 } from "./img/👋.png";
+export { default as icon2 } from "./img/📅.png";
+export { default as icon3 } from "./img/📋.png";
+export { default as icon4 } from "./img/📝.png";
