499e756cd1efcc100b093f1d8b3468fe7fb8c7aa
SEOg-u <seogyeoul1898@gmail.com>
2022-12-28 22:32:25 +0900

    Update Project Settings

--- latty/src/Router.jsx
+++ latty/src/Router.jsx
@@ -5,12 +5,17 @@ import Error from "./components/Error";
 import Practice from "./Practice";
 import SignUp from "./pages/auth/SignUp/SignUp";
 import LogIn from "./pages/auth/LogIn";
+import Header from "./components/Header";
+import Function from "./pages/Function/Function";
 
 const Router = () => {
   return (
     <CookiesProvider>
       <BrowserRouter>
         <Routes>
+          <Route element={<Header />}>
+            <Route path="/function" element={<Function />} />
+          </Route>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="*" element={<Error />} />
