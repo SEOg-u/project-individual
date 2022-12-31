483caf8e23f0cffbb866fb50f39d14d0b282222c
SEOg-u <seogyeoul1898@gmail.com>
2022-12-31 23:15:21 +0900

    Create function/bunchs/style

---
+++ latty/src/components/function/bunchs/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+// styled-components
+export const Wrapper = styled.div`
+  width: 540px;
+`;
+export const FamousSaying = styled.div`
+  height: 220px;
+  background: #ffffff;
+  border: 1px solid #6c7139;
+  border-radius: 10px;
+`;
+export const Top = styled.div``;
+
+export const Writing = styled.div`
+  margin-top: 15px;
+  height: 581px;
+  background: #ffffff;
+  border: 1px solid #6c7139;
+  border-radius: 10px;
+`;
